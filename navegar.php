7b624d6ad7a918ff17cee45ef09bad50c07125bc
LuisaZabala <luisazabala99@gmail.com>
2020-06-03 20:06:29 -0500

    InsertarVisitas

--- navegar.php
+++ navegar.php
@@ -18,7 +18,7 @@
 
           <div class="section-1_card" align="center"><a type="submit" href="Vista/Usuario/ingresarUsuario.php">Agenda Usuarios</a></div>
           <div class="section-1_card" align="center"><a type="submit" href="Vista/Contacto/InsertarContacto.php">Gestion Clientes</a></div>
-          <div class="section-1_card" align="center"><a type="submit" href="">Agenda de Visitas</a></div>
+          <div class="section-1_card" align="center"><a type="submit" href="Vista/Visitas/ingresarVisitas.php">Agenda de Visitas</a></div>
      </section>
      <section class="section-1">
           <div class="section-1_card" align="center"><a type="button" href="vista/Cotizacion/ListarCotizacion.php">Cotizaciones</a></div>
